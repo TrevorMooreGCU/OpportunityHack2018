c55ff4d41ce30f14e009628fc5a230680c0e567f
DESKTOP-DN3TGQJ\carte <carterlrice@gmail.com>
2018-10-21 00:28:53 -0400

    table name services

--- Hackathon/src/com/hackathon/services/business/ITableNameService.java
+++ Hackathon/src/com/hackathon/services/business/ITableNameService.java
@@ -1,5 +1,12 @@
 package com.hackathon.services.business;
 
+import java.util.List;
+
+import com.hackathon.model.EmployeeModel;
+import com.hackathon.model.TableModel;
+
 public interface ITableNameService {
 
+	public List<TableModel> getTableNames();
+	
 }
--- Hackathon/src/com/hackathon/services/business/TableNameService.java
+++ Hackathon/src/com/hackathon/services/business/TableNameService.java
@@ -1,5 +1,27 @@
 package com.hackathon.services.business;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hackathon.model.EmployeeModel;
+import com.hackathon.model.TableModel;
+import com.hackathon.services.data.ISecurityDAO;
+import com.hackathon.services.data.ITableNamesDAO;
+
 public class TableNameService implements ITableNameService {
+
+	ITableNamesDAO tableNamesDAO;
+
+	@Autowired
+	public void setSecurityDAO(ITableNamesDAO dao)
+	{
+		this.tableNamesDAO = dao;
+	}
+	
+	@Override
+	public List<TableModel> getTableNames() {
+		return tableNamesDAO.TableNames();
+	}
 
 }
